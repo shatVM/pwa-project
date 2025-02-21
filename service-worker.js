3b0169eb828ffbb50b007c2675885e1ae0c6aa1b
ShatVM <pzs_shvm@ztu.edu.ua>
2025-02-21 12:21:57 +0200

    add PWA support with service worker and manifest file

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,27 @@
-// This file is intentionally left blank.
+const CACHE_NAME = 'pwa-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/css/styles.css',
+  '/js/script.js',
+  '/img/icon-128x128.png',
+  '/img/icon-512x512.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        return response || fetch(event.request);
+      })
+  );
+});
